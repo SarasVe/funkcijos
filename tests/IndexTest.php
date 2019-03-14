7307c020f1d53df8f8a41008e4951080eec1d4a3
Sarunas <sarunas@whamedia.com>
2019-03-14 08:12:17 +0000

    checkString funkcijai testo loopsas, Order klase, /src/

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -1,5 +1,8 @@
 <?php
-require_once 'index.php';
+namespace tests;
+
+include('./src/index.php');
+
 use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
@@ -8,48 +11,86 @@ class OrderTest extends TestCase
 
     function OrderTest($abc)
     {
-        $this->TestCase($abc);
+        $this->testCheckString($abc);
     }
 
-    function setUp()
+    function setUp(): void
     {
         // create a new instance of String with the
         // string 'abc'
         $this->abc = new Order();
     }
-    function tearDown() {
+    function tearDown(): void
+    {
         // delete your instance
         unset($this->abc);
     }
 
-    public function testCheckString()
+    /**
+     * @dataProvider infoProvider
+     */
+    public function testCheckString($d)
     {
-        foreach ($this as $key => $value) {
-            $this->assertNotNull($value);
+        $check = new Order();
+        $this->assertTrue($d);
+    }
+
+
+    public function infoProvider($data)
+    {
+        global $data, $d;
+        $data = json_decode(json_encode($data),true);
+        $d = $data;
+
+        foreach($d as $k => $v){
+
+            if(is_array($v)){
+
+                foreach($v as $k1 => $v1){
+
+                    $d[$k][$k1] = null;
+                    OrderTest::testCheckString($d);
+                    $d = $data;
+
+                    if(is_array($v1)){
+
+                        foreach($v1 as $k2 => $v2){
+
+                            $d[$k][$k1][$k2] = null;
+                            OrderTest::testCheckString($d);
+                            $d = $data;
+                        }
+                    }
+                }
+            }
         }
     }
 }
-/*
-array(
-	'order' => array(
-		'id' => 947864436667,
-		'name' => 'vardenis',
-		'email' => 'vards@mail.com',
-		'note' => $note,
-		'line_items' => array([
-			'line_item_id' => $lineItem_ID,
-			'variant_id' => $variant_ID,
-			'title' => 't-shirt crazy',
-			'quantity' => 1,
-			'price' => 3.00
-		]),
-		'shipping_address' => array(
-			'last_name' => 'BubLastName', //required
-			'address1' => '123 Ship Street',  //required
-			'phone' => '+37066655544',
-      		'city' => 'Shipsville',  //required
-			'zip' => '41000',  //required
-			'country' => 'France'
-		)
-	)
-);*/
+$data = array(
+    'order' => array(
+        'name' => 'vardenis',
+        'email' => 'vards@mail.com',
+        'note' => 'Gateway: PayPal',
+        'line_items' => array([
+            'line_item_id' => 321312,
+            'variant_id' => 238974,
+            'title' => 't-shirt crazy',
+            'quantity' => 1,
+            'price' => 3.00
+        ]),
+        'shipping_address' => array(
+            'last_name' => 'BubLastName', //required
+            'address1' => '123 Ship Street',  //required
+            'phone' => '+37066655544',
+            'city' => 'Shipsville',  //required
+            'zip' => '41000',  //required
+            'country' => 'France'
+        )
+    )
+);
+//$testas = new OrderTest($data);
+//$result = PHPUnit::run($testas);
+//$test = OrderTest::testCheckString($data);
+//$t = true;
+$test = OrderTest::infoProvider($data);
+//echo $test->testCheckString();
